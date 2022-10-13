792f7dbf9fedd8d74efb58dcf780202ae603fa42
1sol2sol <17gksthf@gmail.com>
2022-10-13 21:42:21 +0900

    feat: post로 핸드폰 번호 인증번호 요청보냄
    요청 보낸 후 인증번호 입력하는 Input 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,43 +1,107 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import { useState } from "react";
-import Button from "../components/button";
-import Input from "../components/input";
+import { useForm } from "react-hook-form";
+import Button from "@components/button";
+import Input from "@components/input";
+import useMutation from "@libs/client/useMutation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+interface LoginForm {
+  phone: string;
+}
+interface LoginMutationResult {
+  ok: boolean;
+}
+interface TokenForm {
+  token: string;
+}
+
+interface MutationResult {
+  ok: boolean;
+}
 
 const Login: NextPage = () => {
-  const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const [login, { loading, data, error }] =
+    useMutation<LoginMutationResult>("/api/users/login");
+  const [confirmToken, { loading:tokenLoading, data:tokenData}] =
+  useMutation<MutationResult>("/api/users/confirm");
+  const {register:tokenRegister, handleSubmit:tokenHandleSubmit} = useForm<TokenForm>();
+
+  const onValid = (validForm: LoginForm) => {
+    if (loading) return;
+    login(validForm);
+  };
+  const onTokenValid = (validForm: TokenForm) => {
+    if(tokenLoading) return;
+    confirmToken(validForm);    
+  };
+
+  const router = useRouter();
+  useEffect(() => {
+    if(tokenData?.ok){
+      router.push("/")
+    }
+  },[tokenData, router])
+
   return (
     <div className="mt-32 px-4">
       <h3 className="text-center text-2xl font-semibold">
         핸드폰 번호로 로그인해주세요.
       </h3>
       <div className="mt-10">
-        <form className="mt-8 flex flex-col space-y-4">
-          <Input
-            name="phone"
-            label="Phone number"
-            type="number"
-            kind="phone"
-            required
-          />
-          <Button text={"인증번호 받기"} />
-        </form>
+        {data?.ok ? (
+          <form
+            onSubmit={tokenHandleSubmit(onTokenValid)}
+            className="flex flex-col mt-8 space-y-4"
+          >
+              <Input
+                register={tokenRegister("token")}
+                name="token"
+                label="인증번호"
+                type="number"
+                required
+              />   
+              <Button text={tokenLoading ? "Loading" : "인증완료"} />
+          </form>
+        ) : (
+          <>
+            <form
+              onSubmit={handleSubmit(onValid)}
+              className="mt-8 flex flex-col space-y-4"
+            >
+              <Input
+                register={register("phone")}
+                name="phone"
+                label="Phone number"
+                type="number"
+                kind="phone"
+                required
+              />
+              <Button text={loading ? "Loading..." : "인증번호 받기"} />
+            </form>
+          </>
+        )}
         <div className="mt-8">
           <div className="relative">
             <div className="absolute w-full border-t border-gray-300" />
             <div className="relative -top-3 text-center ">
               <span className="bg-white px-2 text-sm text-gray-500">
-                Or enter with
+                Or with
               </span>
             </div>
           </div>
           <div className="mt-2 ">
-            <button className="flex w-full items-center space-x-2 justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
-              <Image src="/kakao-logo.svg" width={20} height={20} alt="kakaoLogo"/>
+            <button className="flex w-full items-center justify-center space-x-2 rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
+              <Image
+                src="/kakao-logo.svg"
+                width={20}
+                height={20}
+                alt="kakaoLogo"
+              />
               <span className="text-sm">카카오로 로그인하기</span>
-            </button>  
+            </button>
           </div>
         </div>
       </div>
