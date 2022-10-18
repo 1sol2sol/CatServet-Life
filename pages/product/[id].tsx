d9e176ae2c1266b88244fd81e2974988f8e2392b
1sol2sol <17gksthf@gmail.com>
2022-10-18 15:04:46 +0900

    feat: 상품 상세페이지 좋아요, 관련상품 기능 추가

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,8 +1,38 @@
 import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
+import { useRouter } from "next/router";
+import useSWR, { useSWRConfig } from "swr";
+import Link from "next/link";
+import { Product, User } from "@prisma/client";
+import useUser from "@libs/client/useUser";
+import useMutation from "@libs/client/useMutation";
+import { cls } from "@libs/client/utils";
+
+interface ProductWithUser extends Product {
+  user: User;
+}
+
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+  isLiked: boolean;
+
+}
 
 const ItemDetail: NextPage = () => {
+  const router = useRouter();
+  const {user, isLoading } = useUser();  
+  const {mutate} = useSWRConfig();
+
+  const {data, mutate: boundMutate} = useSWR<ItemDetailResponse>(router.query.id ? `/api/product/${router.query.id}` : null)
+  const [toggleFav] = useMutation(`/api/product/${router.query.id}/fav`)
+  const onFavClick = () => {
+    if(!data) return;
+    boundMutate({...data, isLiked: !data.isLiked}, false);
+    toggleFav({});
+  }
   return (
     <Layout title="" canGoBack>
       <div className="px-4  py-4">
@@ -11,57 +41,80 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer items-center space-x-3 border-t border-b py-3">
             <div className="h-12 w-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">Tom</p>
-              <p className="text-xs font-medium text-gray-500">
-                View profile &rarr;
-              </p>
+              <p className="text-sm font-medium text-gray-700">{data?.product?.user?.nickname}</p>
+              <Link href={`/users/profile/${data?.product?.user?.id}`}>
+                <a className="text-xs font-medium text-gray-500">
+                  View profile &rarr;
+                </a>
+              </Link>
+              {data?.product.user.id  === user?.id ? <button>삭제</button> : null }
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">캣타워</h1>
-            <span className="mt-3 block text-2xl text-gray-900">₩100000</span>
+            <h1 className="text-3xl font-bold text-gray-900">{data?.product?.name}</h1>
+            <span className="mt-3 block text-2xl text-gray-900">₩{data?.product?.price}</span>
             <p className=" my-6 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
-              quia voluptas praesentium ducimus facere assumenda suscipit ipsum
-              nesciunt est dolore ratione natus voluptatem facilis dignissimos
-              alias sunt architecto nisi maiores dicta, repellat ullam!
-              Reprehenderit, beatae atque! Voluptatum, rem? Illo nostrum, cumque
-              voluptate distinctio blanditiis expedita dolorem voluptatibus est
-              sequi sed.
+              {data?.product?.description}
             </p>
             <div className="flex items-center justify-between space-x-2">
               <Button text="판매자에게 연락하기" />
-              <button className="flex items-center justify-center rounded-md p-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
-                <svg
-                  className="h-6 w-6 "
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
+              <button onClick={onFavClick} className={cls(
+                    "p-3 rounded-md flex items-center justify-center hover:bg-gray-100",
+                    data?.isLiked
+                      ? "text-amber-600 hover:text-amber-700"
+                      : "text-gray-400  hover:text-gray-500"
+                  )}>
+                {data?.isLiked ? (
+                    <svg
+                      className="w-6 h-6"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      className="h-6 w-6 "
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                  )}
               </button>
             </div>
           </div>
         </div>
         <div>
           <h2 className="text-2xl font-bold text-gray-900">비슷한 상품</h2>
-          <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
+          <Link
+              href={`/product/${data?.relatedProducts?.map(
+                (product) => product.id
+              )}`}
+            >
+          <div className=" mt-6 grid grid-cols-2 gap-4 cursor-pointer">
+            {data?.relatedProducts?.map((product) => (
+              <div key={product.id}>
                 <div className="mb-4 h-56 w-full bg-slate-300" />
-                <h3 className="-mb-1 text-gray-700">아르르 캣타워</h3>
-                <span className="text-sm font-medium text-gray-900">₩60000</span>
+                <h3 className="-mb-1 text-gray-700">{product.name}</h3>
+                <span className="text-sm font-medium text-gray-900">₩{product.price}</span>
               </div>
             ))}
           </div>
+          </Link>
         </div>
       </div>
     </Layout>
