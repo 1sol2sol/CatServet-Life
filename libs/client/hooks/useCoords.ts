9afa727da5715b47cbbda6c17e8ae420b1c0a2ad
1sol2sol <17gksthf@gmail.com>
2022-10-19 15:01:17 +0900

    feat: 위도, 경도 구하는 hook

---
+++ libs/client/hooks/useCoords.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+interface useCoordsState {
+  longitude: number | null;
+  latitude: number | null;
+}
+
+export default function useCoords() {
+  const [coords, setCoords] = useState<useCoordsState>({
+    latitude: null,
+    longitude: null,
+  });
+  const onSuccess = ({
+    coords: { latitude, longitude },
+  }: GeolocationPosition) => {
+    setCoords({ latitude, longitude });
+  };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(onSuccess);
+  }, []);
+
+  return coords;
+}
