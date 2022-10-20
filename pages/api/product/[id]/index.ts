424a053c307e2f0fe1e3f7d3021d52d176289dd7
1sol2sol <17gksthf@gmail.com>
2022-10-20 14:45:09 +0900

    feat: 상품 삭제 로직 추가

--- pages/api/product/[id]/index.ts
+++ pages/api/product/[id]/index.ts
@@ -7,10 +7,14 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const {query: {id}, session: {user}} = req;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
   const product = await client.product.findUnique({
     where: {
-      id: Number(id)
+      id: Number(id),
     },
     include: {
       user: {
@@ -18,40 +22,49 @@ async function handler(
           id: true,
           nickname: true,
           avatar: true,
-        }
+        },
       },
-    }
-  })
-  const terms = product?.name.split(" ").map(word => ({
-    name: {
-      contains: word,
     },
-  }))
-  const relatedProducts = await client.product.findMany({
-    where: {
-      OR: terms,
-      AND: {
-        id: {
-          not: product?.id
-        }
-      }
-    }
-  })  
-  const isLiked = Boolean(
-    await client.fav.findFirst({
+  });
+  if (req.method === "GET") {
+    const terms = product?.name.split(" ").map((word) => ({
+      name: {
+        contains: word,
+      },
+    }));
+    const relatedProducts = await client.product.findMany({
       where: {
-        productId: product?.id,
-        userId: user?.id,
-      }
-    })
-  )
-  res.json({ok:true, product, isLiked, relatedProducts})
-  
+        OR: terms,
+        AND: {
+          id: {
+            not: product?.id,
+          },
+        },
+      },
+    });
+    const isLiked = Boolean(
+      await client.fav.findFirst({
+        where: {
+          productId: product?.id,
+          userId: user?.id,
+        },
+      })
+    );
+    res.json({ ok: true, product, isLiked, relatedProducts });
+  }
+  if (req.method === "DELETE") {
+    await client.product.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.json({ ok: true });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["GET"],
+    methods: ["GET", "DELETE"],
     handler,
   })
 );
