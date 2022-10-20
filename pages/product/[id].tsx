8ca7f4e7f0c81ef9ea8a0fa4f05defa22f6328e1
1sol2sol <17gksthf@gmail.com>
2022-10-20 14:58:13 +0900

    feat: 상품삭제 요청, 판매자에게 채팅하기 요청

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -4,11 +4,13 @@ import Layout from "@components/layout";
 import { useRouter } from "next/router";
 import useSWR, { useSWRConfig } from "swr";
 import Link from "next/link";
-import { Product, User } from "@prisma/client";
+import { ChatRoom, Product, User } from "@prisma/client";
 import useUser from "@libs/client/hooks/useUser";
 import useMutation from "@libs/client/hooks/useMutation";
 import { cls } from "@libs/client/utils";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -21,10 +23,17 @@ interface ItemDetailResponse {
   isLiked: boolean;
 }
 
+interface MessageResponse {
+  ok: boolean;
+  chatRoom: ChatRoom;
+  isChatRoom?: ChatRoom;
+}
+
 const ItemDetail: NextPage = () => {
   const router = useRouter();
   const { user, isLoading } = useUser();
   const { mutate } = useSWRConfig();
+  const { handleSubmit } = useForm();
 
   const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/product/${router.query.id}` : null
@@ -33,11 +42,35 @@ const ItemDetail: NextPage = () => {
     `/api/product/${router.query.id}/fav`,
     "POST"
   );
+  const [deleteProduct] = useMutation(`/api/product/${router.query.id}`, "DELETE");
+ 
   const onFavClick = () => {
     if (!data) return;
     boundMutate({ ...data, isLiked: !data.isLiked }, false);
     toggleFav({});
   };
+  // 판매자에게 채팅하기
+  const [send, { data: chatRoomData, loading }] = useMutation<MessageResponse>(
+    "/api/chatRoom",
+    "POST"
+  );
+
+  const onVaild = () => {
+    send(router.query.id); 
+  };
+
+  const onDelete = () => {
+    deleteProduct("");
+    router.push("/profile");
+  }
+  useEffect(() => {
+    if (chatRoomData && chatRoomData?.chatRoom) {
+      router.push(`/chats/${chatRoomData.chatRoom?.id}`);
+    } else if (chatRoomData?.isChatRoom) {
+      router.push(`/chats/${chatRoomData.isChatRoom?.id}`);
+    }
+  }, [chatRoomData, router]);
+
   return (
     <Layout title="" canGoBack>
       <div className="px-4  py-4">
@@ -50,7 +83,7 @@ const ItemDetail: NextPage = () => {
               alt="상품사진"
             />
           </div>
-        
+
           <div className="flex cursor-pointer items-center space-x-3 border-t border-b py-3">
             <Image
               width={48}
@@ -59,7 +92,7 @@ const ItemDetail: NextPage = () => {
               src={`https://imagedelivery.net/omEdHMoJ0gXM7Ip-5lbEIQ/${data?.product?.user?.avatar}/avatar`}
               className="h-12 w-12 rounded-full bg-slate-300"
             />
-            <div>
+            <div className="pr-36">
               <p className="text-sm font-medium text-gray-700">
                 {data?.product?.user?.nickname}
               </p>
@@ -68,10 +101,25 @@ const ItemDetail: NextPage = () => {
                   View profile &rarr;
                 </a>
               </Link>
-              {data?.product.user.id === user?.id ? (
-                <button>삭제</button>
-              ) : null}
             </div>
+            {user?.id === data?.product?.user?.id ? 
+            (
+              <div className="flex w-36 space-x-2 mt-5">
+                  <button
+                  onClick={onDelete}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-yellow-800 focus:border-yellow-800 block w-1/2"
+                  >
+                    삭제
+                  </button>
+                  <select
+                    
+                    className="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-yellow-800 focus:border-yellow-800 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-800 dark:focus:border-yellow-800"
+                  >
+                    <option value={"판매중"}>판매중</option>
+                    <option value={"판매완료"}>판매완료</option>
+                  </select>
+                </div>
+            ) : null}
           </div>
           <div className="mt-5">
             <h1 className="text-3xl font-bold text-gray-900">
@@ -81,53 +129,59 @@ const ItemDetail: NextPage = () => {
               ₩{data?.product?.price}
             </span>
             <p className=" my-6 text-gray-700">{data?.product?.description}</p>
-            <div className="flex items-center justify-between space-x-2">
-              <Button text="판매자에게 연락하기" />
-              <button
-                onClick={onFavClick}
-                className={cls(
-                  "flex items-center justify-center rounded-md p-3 hover:bg-gray-100",
-                  data?.isLiked
-                    ? "text-amber-600 hover:text-amber-700"
-                    : "text-gray-400  hover:text-gray-500"
-                )}
-              >
-                {data?.isLiked ? (
-                  <svg
-                    className="h-6 w-6"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                ) : (
-                  <svg
-                    className="h-6 w-6 "
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
+            {user?.id !== data?.product?.user?.id ? (
+              <div className="flex items-center justify-between space-x-2">
+                <form className="w-full" onSubmit={handleSubmit(onVaild)}>
+                  <Button
+                    text={loading ? "Loading..." : "판매자에게 채팅하기"}
+                  />
+                </form>
+                <button
+                  onClick={onFavClick}
+                  className={cls(
+                    "flex items-center justify-center rounded-md p-3 hover:bg-gray-100",
+                    data?.isLiked
+                      ? "text-yellow-700 hover:text-yellow-800"
+                      : "text-gray-400  hover:text-gray-500"
+                  )}
+                >
+                  {data?.isLiked ? (
+                    <svg
+                      className="h-6 w-6"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      className="h-6 w-6 "
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            ) : null}
           </div>
         </div>
         <div>
-          <h2 className="text-2xl font-bold text-gray-900">비슷한 상품</h2>
+          <h2 className="text-xl font-bold text-gray-800">이런 상품은 어때요?</h2>
           <Link
             href={`/product/${data?.relatedProducts?.map(
               (product) => product.id
