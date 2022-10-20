1947c4c305c25e5042adb33e44bd02c7a25e49bd
1sol2sol <17gksthf@gmail.com>
2022-10-20 14:59:41 +0900

    chore: 프로필 이미지 경로 추가

--- components/message.tsx
+++ components/message.tsx
@@ -1,9 +1,10 @@
 import { cls } from "@libs/client/utils";
+import Image from "next/image";
 
 interface MessageProps {
   message: string;
   reversed?: boolean;
-  avatarUrl?: string;
+  avatarUrl?: string | null;
 }
 
 export default function Message({
@@ -15,11 +16,18 @@ export default function Message({
     <div
       className={cls(
         "flex  items-start",
-        reversed ? "flex-row-reverse space-x-reverse" : "space-x-2"
+        reversed ? "flex-row-reverse space-x-reverse space-x-2" : "space-x-2"
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
-      <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+      <Image
+        width={40}
+        height={40}
+        alt="profile"
+        src={`https://imagedelivery.net/omEdHMoJ0gXM7Ip-5lbEIQ/${avatarUrl}/avatar`}
+        className="z-0 rounded-full bg-gray-300"
+        quality={100}
+      />{" "}
+      <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
         <p>{message}</p>
       </div>
     </div>
