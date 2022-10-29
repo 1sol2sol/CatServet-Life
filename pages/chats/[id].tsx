e56a96a712d39ac6e02aae2e64965adbe97dafd1
1sol2sol <17gksthf@gmail.com>
2022-10-29 20:33:35 +0900

    chore: 타이틀 추가

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -74,7 +74,7 @@ const ChatDetail: NextPage = () => {
     send(form);
   };
   return (
-    <Layout canGoBack title={data?.chatRoom?.sellerId === user?.id ? data?.chatRoom.buyer.nickname : data?.chatRoom.seller.nickname}>
+    <Layout seoTitle="채팅" canGoBack title={data?.chatRoom?.sellerId === user?.id ? data?.chatRoom.buyer.nickname : data?.chatRoom.seller.nickname}>
       <div className="space-y-4 py-10 px-4 pb-16">
         {data?.chatRoom?.messages.map((message) => (
           <Message
