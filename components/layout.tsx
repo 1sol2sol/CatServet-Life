59c173169f3a8a51625f7858e4d0cf4fc7af07cd
1sol2sol <17gksthf@gmail.com>
2022-10-29 19:52:28 +0900

    chore:  Logo 크기 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -101,7 +101,7 @@ export default function Layout({
         {logo ? (
           <div className="flex w-full justify-between">
           <div className="flex items-center">
-            <Image src="/cat-servant.png" width={100} height={35}  alt="catLogo"/>
+            <Image src="/cat-servant.png" width={100} height={40}  alt="catLogo"/>
           </div>
           <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-yellow-800 items-center rounded-full px-2 hover:bg-yellow-700" onClick={onSearchClick}>
             <svg aria-hidden="true" className="w-5 h-5 text-yellow-900 hover:text-white dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
