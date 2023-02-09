65c1e3342f2c5b346b46f2c650b1a4b97b367b30
1sol2sol <17gksthf@gmail.com>
2023-02-09 15:42:03 +0900

    fix: 배포 버그 수정

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 import { NextRequest, NextFetchEvent, userAgent } from "next/server";
 import { NextResponse } from "next/server";
 export function middleware(req: NextRequest, ev:NextFetchEvent){  
-  const ua = userAgent(req);
+  // const ua = userAgent(req);
   
-  if(!req.url.includes("/api")){
-    if(!req.url.includes("/enter") && !req.cookies.get("catservantsession")){
-      return NextResponse.redirect(new URL("/enter/login", req.url))
-    }
-  }
+  // if(!req.url.includes("/api")){
+  //   if(!req.url.includes("/enter") && !req.cookies.get("catservantsession")){
+  //     return NextResponse.redirect(new URL("/enter", req.url))
+  //   }
+  // }
 }
+
+export const config = {
+  matcher: "/",
+  };
